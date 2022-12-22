b9f27d94599973a859a242ddd8e910f3f0c340a2
natrajkalyan <natrajkalyan@gmail.com>
2022-12-22 15:46:34 +0530

    Get Tasks from API

--- able-3/src/components/Test.js
+++ able-3/src/components/Test.js
@@ -1,15 +1,64 @@
 import React from "react";
-import { Cache, Auth } from "aws-amplify";
+import { Auth } from "aws-amplify";
+import { Button } from "@aws-amplify/ui-react";
 
 export class Test extends React.Component {
   constructor(props) {
     super(props);
-    Auth.currentAuthenticatedUser().then((user) =>
-      console.log(user.signInUserSession.idToken.jwtToken)
-    );
+    this.state = { token: "", tasks: [] };
   }
+
+  componentDidMount() {
+    Auth.currentAuthenticatedUser().then((user) => {
+      this.setState({ token: user.signInUserSession.idToken.jwtToken });
+    });
+  }
+
+  getTasks = () => {
+    console.log(this.state.token);
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", this.state.token);
+
+    fetch("https://732dte6goh.execute-api.ap-south-1.amazonaws.com/dev/tasks", {
+      method: "GET",
+      headers: myHeaders,
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error("Something went wrong on api server!");
+        }
+      })
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          tasks: response.tasks,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   render() {
-    return <>Hello</>;
+    let tasks =
+      this.state.tasks &&
+      this.state.tasks.map((task) => {
+        return (
+          <li>
+            {task.taskId} - {task.taskDesc}
+          </li>
+        );
+      });
+
+    return (
+      <>
+        <Button onClick={this.getTasks}>Get Tasks</Button>
+        {tasks}
+      </>
+    );
   }
 }
 
